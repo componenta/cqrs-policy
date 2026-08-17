c8c5e471768728af98be524a85af9720a80f8f92
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-17 15:39:41 +0200

    Build command policy middleware from explicit actor resolver

--- src/Command/Factory/PolicyMiddlewareFactory.php
+++ src/Command/Factory/PolicyMiddlewareFactory.php
@@ -4,54 +4,34 @@ declare(strict_types=1);
 
 namespace Componenta\CQRS\Command\Factory;
 
+use Componenta\Config\ContainerValue;
 use Componenta\CQRS\Command\Middleware\PolicyMiddleware;
 use Componenta\CQRS\Resolver\ActionIdResolver;
 use Componenta\CQRS\Resolver\ActionIdResolverInterface;
-use Componenta\Policy\Actor\ActorProviderInterface;
+use Componenta\CQRS\Resolver\ActorResolver;
 use Componenta\Policy\PolicyEnforcer;
-use LogicException;
-use Psr\Container\ContainerInterface;
 
 final class PolicyMiddlewareFactory
 {
-    public function __invoke(ContainerInterface $container): PolicyMiddleware
+    public function __invoke(ContainerValue $container): PolicyMiddleware
     {
-        $enforcer = $container->get(PolicyEnforcer::class);
+        $resolver = $container->has(ActionIdResolverInterface::class)
+            ? $container->get(
+                ActionIdResolverInterface::class,
+                ActionIdResolverInterface::class,
+            )
+            : new ActionIdResolver();
 
-        if (!$enforcer instanceof PolicyEnforcer) {
-            throw new LogicException(sprintf(
-                'Container entry "%s" must be a %s instance.',
+        return new PolicyMiddleware(
+            enforcer: $container->get(
                 PolicyEnforcer::class,
                 PolicyEnforcer::class,
-            ));
-        }
-
-        $actorProvider = null;
-        if ($container->has(ActorProviderInterface::class)) {
-            $actorProvider = $container->get(ActorProviderInterface::class);
-
-            if (!$actorProvider instanceof ActorProviderInterface) {
-                throw new LogicException(sprintf(
-                    'Container entry "%s" must implement %s.',
-                    ActorProviderInterface::class,
-                    ActorProviderInterface::class,
-                ));
-            }
-        }
-
-        $resolver = new ActionIdResolver();
-        if ($container->has(ActionIdResolverInterface::class)) {
-            $resolver = $container->get(ActionIdResolverInterface::class);
-
-            if (!$resolver instanceof ActionIdResolverInterface) {
-                throw new LogicException(sprintf(
-                    'Container entry "%s" must implement %s.',
-                    ActionIdResolverInterface::class,
-                    ActionIdResolverInterface::class,
-                ));
-            }
-        }
-
-        return new PolicyMiddleware($enforcer, $actorProvider, $resolver);
+            ),
+            actors: $container->get(
+                ActorResolver::class,
+                ActorResolver::class,
+            ),
+            resolver: $resolver,
+        );
     }
 }
