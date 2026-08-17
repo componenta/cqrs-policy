fae8c91bd1889783a6a3495c7f14161e6c84796f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-17 17:31:22 +0200

    Allow repositories to return any policy actor object

--- src/Policy/Transport/ActorRepositoryInterface.php
+++ src/Policy/Transport/ActorRepositoryInterface.php
@@ -5,10 +5,9 @@ declare(strict_types=1);
 namespace Componenta\CQRS\Policy\Transport;
 
 use Componenta\Identity\UuidInterface;
-use Componenta\Policy\Actor\ActorInterface;
 
-/** Loads the current actor state used to restore transported commands. */
+/** Loads the current policy actor used to restore transported commands. */
 interface ActorRepositoryInterface
 {
-    public function findByUuid(UuidInterface $uuid): ?ActorInterface;
+    public function findByUuid(UuidInterface $uuid): ?object;
 }
