9ef9656176c06d8505230be74343649e7eed821a
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-17 22:05:10 +0200

    Build specialized actor serializer without fallback

--- src/Policy/Transport/Factory/ActorAwareJsonCommandSerializerFactory.php
+++ src/Policy/Transport/Factory/ActorAwareJsonCommandSerializerFactory.php
@@ -5,21 +5,18 @@ declare(strict_types=1);
 namespace Componenta\CQRS\Policy\Transport\Factory;
 
 use Componenta\Config\ContainerValue;
-use Componenta\CQRS\Command\Transport\CommandSerializerInterface;
-use Componenta\CQRS\Command\Transport\JsonCommandSerializer;
 use Componenta\CQRS\Policy\Transport\ActorAwareJsonCommandSerializer;
 use Componenta\CQRS\Policy\Transport\ActorRepositoryInterface;
 
 final class ActorAwareJsonCommandSerializerFactory
 {
-    public function __invoke(ContainerValue $container): CommandSerializerInterface
+    public function __invoke(ContainerValue $container): ActorAwareJsonCommandSerializer
     {
         return new ActorAwareJsonCommandSerializer(
-            actors: $container->get(
+            $container->get(
                 ActorRepositoryInterface::class,
                 ActorRepositoryInterface::class,
             ),
-            fallback: new JsonCommandSerializer(),
         );
     }
 }
