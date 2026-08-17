1b781fa70dd907cb72a4c5e1ad0b6ada595e119a
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-17 15:41:15 +0200

    Test actor resolver registration

--- tests/ConfigProviderTest.php
+++ tests/ConfigProviderTest.php
@@ -8,13 +8,15 @@ use Componenta\CQRS\Command\Middleware\PolicyMiddleware as CommandPolicyMiddlewa
 use Componenta\CQRS\Policy\ConfigProvider;
 use Componenta\CQRS\Query\Factory\PolicyMiddlewareFactory as QueryPolicyMiddlewareFactory;
 use Componenta\CQRS\Query\Middleware\PolicyMiddleware as QueryPolicyMiddleware;
+use Componenta\CQRS\Resolver\ActorResolver;
 
-it('registers policy middleware factories', function (): void {
+it('registers policy middleware factories and the actor resolver', function (): void {
     $config = (new ConfigProvider())();
-    $factories = $config[DependencyConfigKey::DEPENDENCIES][DependencyConfigKey::FACTORIES];
+    $dependencies = $config[DependencyConfigKey::DEPENDENCIES];
 
-    expect($factories)->toMatchArray([
+    expect($dependencies[DependencyConfigKey::FACTORIES])->toMatchArray([
         CommandPolicyMiddleware::class => CommandPolicyMiddlewareFactory::class,
         QueryPolicyMiddleware::class => QueryPolicyMiddlewareFactory::class,
-    ]);
+    ])->and($dependencies[DependencyConfigKey::INVOKABLES])
+        ->toContain(ActorResolver::class);
 });
