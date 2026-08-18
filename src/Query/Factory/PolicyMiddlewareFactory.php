23e30c4e8d42774088536e44ee0d0ee4e97bbde2
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-18 14:53:14 +0200

    Remove ambient query actor provider

--- src/Query/Factory/PolicyMiddlewareFactory.php
+++ src/Query/Factory/PolicyMiddlewareFactory.php
@@ -8,24 +8,18 @@ use Componenta\Config\ContainerValue;
 use Componenta\CQRS\Query\Middleware\PolicyMiddleware;
 use Componenta\CQRS\Resolver\ActionIdResolver;
 use Componenta\CQRS\Resolver\ActionIdResolverInterface;
-use Componenta\Policy\Actor\ActorProviderInterface;
 use Componenta\Policy\PolicyEnforcer;
 
 final class PolicyMiddlewareFactory
 {
     public function __invoke(ContainerValue $container): PolicyMiddleware
     {
-        $actorProvider = $container->has(ActorProviderInterface::class)
-            ? $container->get(ActorProviderInterface::class, ActorProviderInterface::class)
-            : null;
-
         $resolver = $container->has(ActionIdResolverInterface::class)
             ? $container->get(ActionIdResolverInterface::class, ActionIdResolverInterface::class)
             : new ActionIdResolver();
 
         return new PolicyMiddleware(
             enforcer: $container->get(PolicyEnforcer::class, PolicyEnforcer::class),
-            actorProvider: $actorProvider,
             resolver: $resolver,
         );
     }
